25726e79365b8d7db8a805c207613d6654fae09f
vinicius <vnics.silva@gmail.com>
2026-04-08 22:47:04 -0300

    Utilização de um arquivo de ambiente para armazenar a conexão com o banco

--- app/db/db.py
+++ app/db/db.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://su:@#M3rid@0101@localhost/openwallet"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
