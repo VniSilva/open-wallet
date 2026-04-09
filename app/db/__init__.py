7ac4c4f3fa122027666fb7f93307f9c6a5975eb6
vinicius <vnics.silva@gmail.com>
2026-04-08 22:34:16 -0300

    Adicionando .gitignore

---
+++ app/db/__init__.py
@@ -0,0 +1,1 @@
+from db import engine, BASE, get_db
--- app/db/db.py
+++ app/db/db.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_url, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://su:@#M3rid@0101@localhost/openwallet"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
